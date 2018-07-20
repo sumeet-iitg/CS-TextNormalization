78331a10ff314b1d3467aba7c60712f1fe73d194
sumeet-iitg <sumeet.iitg@gmail.com>
2018-07-20 17:28:10 -0400

    Fix for no grad in torch 0.3

--- Equilid/evaluator.py
+++ Equilid/evaluator.py
@@ -43,23 +43,22 @@ class Evaluator(object):
         tgt_vocab = data.fields['tgt'].vocab
         pad = tgt_vocab.stoi[data.fields['tgt'].pad_token]
 
-        with torch.no_grad():
-            for batch in batch_iterator:
-                input_variables, input_lengths  = getattr(batch, 'src')
-                target_variables = getattr(batch, 'tgt')
+        for batch in batch_iterator:
+            input_variables, input_lengths  = getattr(batch, 'src')
+            target_variables = getattr(batch, 'tgt')
 
-                decoder_outputs, decoder_hidden, other = model(input_variables, input_lengths.tolist(), target_variables)
+            decoder_outputs, decoder_hidden, other = model(input_variables, input_lengths.tolist(), target_variables)
 
-                # Evaluation
-                seqlist = other['sequence']
-                for step, step_output in enumerate(decoder_outputs):
-                    target = target_variables[:, step + 1]
-                    loss.eval_batch(step_output.view(target_variables.size(0), -1), target)
+            # Evaluation
+            seqlist = other['sequence']
+            for step, step_output in enumerate(decoder_outputs):
+                target = target_variables[:, step + 1]
+                loss.eval_batch(step_output.view(target_variables.size(0), -1), target)
 
-                    non_padding = target.ne(pad)
-                    correct = seqlist[step].view(-1).eq(target).masked_select(non_padding).sum().item()
-                    match += correct
-                    total += non_padding.sum().item()
+                non_padding = target.ne(pad)
+                correct = seqlist[step].view(-1).eq(target).masked_select(non_padding).sum().item()
+                match += correct
+                total += non_padding.sum().item()
 
         if total == 0:
             accuracy = float('nan')
