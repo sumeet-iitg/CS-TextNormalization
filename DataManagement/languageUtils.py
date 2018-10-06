b4f9c7350c582ccaf5fe699f1a34eb7868e47ba3
sumeet-iitg <sumeet.iitg@gmail.com>
2018-10-06 02:03:46 -0400

    Now we can load languages from a single config file

--- DataManagement/languageUtils.py
+++ DataManagement/languageUtils.py
@@ -2,29 +2,46 @@
 import re
 from DataManagement.constants import ANNOT_REGEX, LITCM_CODE_TO_UNIV_CODE
 import codecs
+import os
+import json
 
-class language(object):
-    def __init__(self, languageCode, spellChecker=None):
-        # dictionary of words in this language
-        self.code = languageCode
-        self.word2Idx = {}
-        self.idx2Word = {}
-        self.stopWordIds = []
+__language_map__ = {}
 
-    def addWordToDict(self, word, isStopWord = False):
-        if not word in self.word2Idx.keys():
-            lastDictPosn = len(self.word2Idx.keys()) - 1
-            self.word2Idx[word] = lastDictPosn+1
-            self.idx2Word[lastDictPosn+1] = word
-            if isStopWord:
-                self.stopWordIds.append(lastDictPosn+1)
+def load_lexicon(languageObject):
+    word2Idx = {}
+    idx2Word = {}
+    stopWordIds = []
+    vocab_path = getattr(languageObject, 'vocab_path', None)
+    if not vocab_path is None and os.path.exists(vocab_path):
+        with codecs.open(vocab_path, 'r', 'utf-8') as fp:
+            lexicon = fp.read().split()
+        stopWords = []
+        stop_word_path = getattr(languageObject, 'stop_word_path', None)
+        if not stop_word_path is None and os.path.exists(stop_word_path):
+            with codecs.open(stop_word_path, 'r', 'utf-8') as fp:
+                stopWords = fp.read().split()
 
-    '''
-    If the word is in lexicon returns true, else false
-    '''
-    def basicSpellChecker(self, text):
-        return True if text in self.word2Idx.keys() else False
+        for word in lexicon:
+            if not word in word2Idx.keys():
+                lastDictPosn = len(word2Idx.keys()) - 1
+                word2Idx[word] = lastDictPosn + 1
+                idx2Word[lastDictPosn + 1] = word
+                if word in stopWords:
+                    stopWordIds.append(lastDictPosn + 1)
+    setattr(languageObject, 'word2Idx', word2Idx)
+    setattr(languageObject, 'idx2Word', idx2Word)
+    setattr(languageObject, 'stopWordIds', stopWordIds)
 
+def languageLoader(languageConfigFile):
+    with open(languageConfigFile) as json_data:
+        languages = json.load(json_data)
+        for langJSON in languages["languageObjects"]:
+            # create a new language type from each of these JSONs
+            langName, langProperties = next(iter(langJSON.keys())), dict(next(iter(langJSON.values())))
+            # dynamically created language object
+            langObject = type(langName, (object,), langProperties)
+            load_lexicon(langObject)
+            __language_map__[langObject.code] = langObject
 
 class languageIdentifier(object):
     def __init__(self, languageSet):
@@ -36,47 +53,23 @@ class languageIdentifier(object):
     def detectLanguageInWord(self, word):
         return NotImplementedError
 
+
 class indicLangIdentifier(languageIdentifier):
     def __init__(self,languageSet):
         super().__init__(languageSet)
 
+
+# TODO: Make this languages directly callable by their names so that we don't create objects beforehand
 def polyglot_SpellChecker(languageAnnotated_Text):
     correctedWords = []
     for wordLangPair in languageAnnotated_Text.split():
         word, lang = wordLangPair.split('\\')
-        # if this word is annotation tag or has  then don't spell check
-        if not re.search(ANNOT_REGEX, word) and lang.lower() in LITCM_CODE_TO_UNIV_CODE.keys():
-            langObject = languageMap[LITCM_CODE_TO_UNIV_CODE[lang.lower()]]
-            if not langObject.basicSpellChecker(word):
-                word = "<unk>"
+        # if this word is an annotation tag or isn't in the language map then don't spell check
+        if not re.search(ANNOT_REGEX, word) and lang.lower() in __language_map__.keys():
+            langObject = __language_map__[lang.lower()]
+            # if not langObject.basicSpellChecker(word):
+            #     word = "<unk>"
         correctedWords.append(word + '\\' + lang)
     return " ".join(correctedWords)
 
-def getEnglish():
-    topWordsFile = "C:\\Users\\Sumeet Singh\\PycharmProjects\\TextNormalization\\CS-TextNormalization\\datasets\\top1000-English.txt"
-    englishLanguage = language("en")
-    with codecs.open(topWordsFile, 'r', 'utf-8') as fp:
-        lexicon = fp.read().split()
 
-    stopWords = []
-    # create language dictionaries
-    for word in lexicon:
-        englishLanguage.addWordToDict(word, word in stopWords)
-    return englishLanguage
-
-def getHindi():
-    topWordsFile = "C:\\Users\\Sumeet Singh\\PycharmProjects\\TextNormalization\\CS-TextNormalization\\datasets\\top1000-Hindi.txt"
-    hindiLanguage = language("hi")
-    with codecs.open(topWordsFile, 'r', 'utf-8') as fp:
-        lexicon = fp.read().split()
-    stopWords = []
-    for word in lexicon:
-        hindiLanguage.addWordToDict(word, word in stopWords)
-    return hindiLanguage
-
-englishLanguage = getEnglish()
-hindiLanguage = getHindi()
-
-# after getting language codes use this map to get the language specific objects
-# which will help in spell checking
-languageMap = {"en":englishLanguage, "hi":hindiLanguage}
