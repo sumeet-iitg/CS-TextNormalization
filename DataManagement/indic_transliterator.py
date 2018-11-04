cc9715151959f5f81e51a049cba6e42723828a41
sumeet-iitg <sumeet.iitg@gmail.com>
2018-11-04 01:41:25 -0400

    Removing TextCleanser submodule.

--- DataManagement/indic_transliterator.py
+++ DataManagement/indic_transliterator.py
@@ -3,11 +3,11 @@ from indic_transliteration import sanscript
 from indic_transliteration.sanscript import SchemeMap, SCHEMES, transliterate
 import codecs
 
-# with codecs.open(
-#         "C:\\Users\\SumeetSingh\\PycharmProjects\\TextNormalization\\CS-TextNormalization\\datasets\\top1000-Hindi-translit.txt",'r', encoding='utf-8') as fr:
-#     with codecs.open("C:\\Users\\SumeetSingh\\PycharmProjects\\TextNormalization\\CS-TextNormalization\\datasets\\top1000-Hindi-back-translit.txt", 'w', encoding='utf-8') as fw:
-#         for line in fr.readlines():
-#             data = line.split()
-#             if len(data) > 0:
-#                 devnagri = transliterate(data[0], sanscript.ITRANS, sanscript.DEVANAGARI)
-#                 fw.write(devnagri+'\n')
+with codecs.open(
+        "C:\\Users\\SumeetSingh\\PycharmProjects\\TextNormalization\\CS-TextNormalization\\datasets\\top1000-Hindi-romanized.txt",'r', encoding='utf-8') as fr:
+    with codecs.open("C:\\Users\\SumeetSingh\\PycharmProjects\\TextNormalization\\CS-TextNormalization\\datasets\\top1000-Hindi-BackTranslit.txt", 'w', encoding='utf-8') as fw:
+        for line in fr.readlines():
+            data = line.split()
+            if len(data) > 0:
+                translit = transliterate(data[0], sanscript.ITRANS,sanscript.DEVANAGARI)
+                fw.write(translit+'\n')
